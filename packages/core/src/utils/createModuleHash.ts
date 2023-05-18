ad38a82e5aaac7bc1f57b49766a064726e302419
Sebastian Lorenz <fubhy@fubhy.com>
2023-05-18 23:30:54 +0200

    feat: adding module hasing (wip)

--- packages/core/src/utils/createModuleHash.ts
+++ packages/core/src/utils/createModuleHash.ts
@@ -1,48 +1,67 @@
 import type { Module, Module_Input, Modules } from "../proto/sf/substreams/v1/modules_pb.js";
 
-export async function createBinaryHash(buffer: Buffer) {
-  const hash = await global.crypto.subtle.digest("SHA-1", buffer);
-  return Buffer.from(hash).toString("hex");
+export async function createBinaryHash(array: Uint8Array) {
+  const hash = await globalThis.crypto.subtle.digest("SHA-1", array);
+  return toHex(new Uint8Array(hash));
+}
+
+export async function toHex(array: Uint8Array) {
+  return Array.from(array, (value) => value.toString(16).padStart(2, "0")).join("");
 }
 
 export async function createModuleHash(modules: Modules, module: Module) {
-  const buffer = await hashModule(modules, module);
-  return createBinaryHash(buffer);
+  const array = await hashModule(modules, module);
+  return createBinaryHash(array);
 }
 
 export async function hashModule(modules: Modules, module: Module) {
+  const encoder = new TextEncoder();
   const chunks: Uint8Array[] = [];
-  chunks.push(Buffer.from("initial_block"));
-  const initialBlockBytes = Buffer.alloc(8);
-  initialBlockBytes.writeBigUInt64LE(module.initialBlock);
-  chunks.push(initialBlockBytes);
 
-  chunks.push(Buffer.from("kind"));
-  chunks.push(Buffer.from(moduleKind(module)));
+  chunks.push(encoder.encode("initial_block"));
+  chunks.push(encodeInitialBlock(module));
 
-  chunks.push(Buffer.from("binary"));
+  chunks.push(encoder.encode("kind"));
+  chunks.push(encoder.encode(moduleKind(module)));
+
+  chunks.push(encoder.encode("binary"));
   const binary = modules.binaries[module.binaryIndex];
   if (binary !== undefined) {
-    chunks.push(Buffer.from(binary.type));
+    chunks.push(encoder.encode(binary.type));
     chunks.push(binary.content);
   }
 
-  chunks.push(Buffer.from("inputs"));
+  chunks.push(encoder.encode("inputs"));
   for (const input of module.inputs) {
-    chunks.push(Buffer.from(inputType(input)));
-    chunks.push(Buffer.from(inputValue(input)));
+    chunks.push(encoder.encode(inputType(input)));
+    chunks.push(encoder.encode(inputValue(input)));
   }
 
-  chunks.push(Buffer.from("ancestors"));
+  chunks.push(encoder.encode("ancestors"));
   // TODO: Implement module graph.
   // for (const ancestor of getAncestors(modules, module)) {
   //   chunks.push(await hashModule(modules, ancestor));
   // }
 
-  chunks.push(Buffer.from("entrypoint"));
-  chunks.push(Buffer.from(module.name));
+  chunks.push(encoder.encode("entrypoint"));
+  chunks.push(encoder.encode(module.name));
 
-  return Buffer.concat(chunks);
+  const length = chunks.reduce((sum, chunk) => sum + chunk.length, 0);
+  const array = new Uint8Array(length);
+
+  let offset = 0;
+  for (const chunk of chunks) {
+    array.set(chunk, offset);
+    offset += chunk.length;
+  }
+
+  return array;
+}
+
+function encodeInitialBlock(module: Module) {
+  const array = new Uint8Array(8);
+  new DataView(array.buffer).setBigUint64(0, module.initialBlock, true);
+  return array;
 }
 
 function moduleKind(module: Module): string {
