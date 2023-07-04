8507e5f439d2d0738ec2433c1badd0e1b0310314
Sebastian Lorenz <fubhy@fubhy.com>
2023-07-04 09:52:00 +0200

    docs: add manifest example

---
+++ examples/example-node-manifest/src/index.ts
@@ -0,0 +1,47 @@
+import { createGrpcTransport } from "@bufbuild/connect-node";
+import {
+  createAuthInterceptor,
+  createRegistry,
+  createRequest,
+  isEmptyMessage,
+  streamBlocks,
+  unpackMapOutput,
+} from "@substreams/core";
+import { readPackage } from "@substreams/manifest";
+import * as url from "node:url";
+
+if (typeof process.env.SUBSTREAMS_API_TOKEN !== "string") {
+  throw new Error('Missing "SUBSTREAMS_API_TOKEN" environment variable');
+}
+
+const TOKEN = process.env.SUBSTREAMS_API_TOKEN;
+const SUBSTREAM = url.fileURLToPath(new URL("../substream/substreams.yaml", import.meta.url));
+const MODULE = "map_pools_created";
+
+const substream = await readPackage(SUBSTREAM);
+const registry = createRegistry(substream);
+
+// The `grpc` transport is usually preferable in environments that support it (e.g. Node.js).
+// In browser environments or alternative runtimes, the `connect` transport should be used.
+const transport = createGrpcTransport({
+  baseUrl: "https://mainnet.eth.streamingfast.io",
+  httpVersion: "2",
+  interceptors: [createAuthInterceptor(TOKEN)],
+  jsonOptions: {
+    typeRegistry: registry,
+  },
+});
+
+const request = createRequest({
+  substreamPackage: substream,
+  outputModule: MODULE,
+  productionMode: false, // Set to `true` in production.
+  stopBlockNum: "+10000", // Stream the first 10000 blocks. Will follow chain head if not set.
+});
+
+for await (const response of streamBlocks(transport, request)) {
+  const output = unpackMapOutput(response.response, registry);
+  if (output !== undefined && !isEmptyMessage(output)) {
+    console.dir(output.toJson({ typeRegistry: registry }));
+  }
+}
