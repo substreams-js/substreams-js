db70b53fc00e3e0d75cc868a38f47f6aebcdb35d
Sebastian Lorenz <fubhy@fubhy.com>
2023-07-10 17:19:18 +0200

    fix: added error handling for `fetchSubstream`

---
+++ .changeset/small-kids-share.md
@@ -0,0 +1,5 @@
+---
+"@substreams/core": patch
+---
+
+Added error handling when fetching packages
--- packages/core/src/utils/fetchSubstream.ts
+++ packages/core/src/utils/fetchSubstream.ts
@@ -1,9 +1,13 @@
 import { createSubstream } from "./createSubstream.js";
 
 export async function fetchSubstream(...args: Parameters<typeof fetch>) {
-  const buffer = await fetch(...args)
-    .then((response) => response.blob())
-    .then((blob) => blob.arrayBuffer());
+  const response = await fetch(...args);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch substream (code ${response.status}): ${response.statusText}`);
+  }
+
+  const blob = await response.blob();
+  const buffer = await blob.arrayBuffer();
 
   return createSubstream(buffer);
 }
