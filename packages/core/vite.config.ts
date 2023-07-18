9d5dcde39be8bc15efc9ef1b1d3adb2fd3557711
Sebastian Lorenz <fubhy@fubhy.com>
2023-07-18 17:54:58 +0200

    tests: fix tests in ci

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,7 +2,7 @@ import aliases from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: process.env.CI ? [] : [aliases()],
+  plugins: [aliases()],
   test: {
     testTimeout: 30000,
     coverage: {
--- packages/mermaid/vite.config.ts
+++ packages/mermaid/vite.config.ts
@@ -2,7 +2,7 @@ import aliases from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: process.env.CI ? [] : [aliases()],
+  plugins: [aliases()],
   test: {
     testTimeout: 30000,
     // TODO: Cheeky... Let's remove this asap, heh.
