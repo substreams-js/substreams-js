4382ecdff8573c367147605832697552d2585884
Sebastian Lorenz <fubhy@fubhy.com>
2023-07-23 16:00:53 +0200

    chore: fix local development package linking

--- apps/playground/next.config.js
+++ apps/playground/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["@substreams/core", "@substreams/react", "@substreams/mermaid"],
+  webpack: (config) => {
+    config.resolve.extensionAlias = {
+      ".js": [".js", ".ts"],
+      ".jsx": [".jsx", ".tsx"],
+    };
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
