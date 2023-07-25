f01c0d0034c999d5847acd788274f51fb2cab8fe
Sebastian Lorenz <fubhy@fubhy.com>
2023-07-25 16:21:26 +0200

    fix: description

--- apps/playground/src/lib/config.ts
+++ apps/playground/src/lib/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   name: "Substreams",
   url: "https://substreams.vercel.app",
-  description: "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  description: "Substreams is a powerful blockchain indexing technology, developed for The Graph Network.",
   links: {
     twitter: "https://twitter.com/thefubhy",
     github: "https://github.com/substreams-js/substreams-js",
