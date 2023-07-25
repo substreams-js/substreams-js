dc85e5bbcf02ca1a9ea89d9a1e4b2ac0e5cbc49b
Sebastian Lorenz <fubhy@fubhy.com>
2023-07-25 16:03:11 +0200

    chore: remove gui app

--- apps/gui/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { App } from "./components/app.js";
-import ReactDOM from "react-dom/client";
-
-// rome-ignore lint/style/noNonNullAssertion: this is guaranteed in index.html
-const root = document.getElementById("root")!;
-
-ReactDOM.createRoot(root).render(<App />);
--- apps/gui/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import aliases from "vite-tsconfig-paths";
-
-export default defineConfig({
-  plugins: [aliases(), react()],
-  envPrefix: "SUBSTREAMS_",
-});
