18e423a3e15e4d6b07207514195f83d8a0a3a4f7
Sebastian Lorenz <fubhy@fubhy.com>
2023-12-11 18:03:28 +0100

    refactor: restructure command

--- examples/example-sink-file/src/commands/run.ts
+++ examples/example-sink-file/src/commands/run.ts
@@ -7,65 +7,61 @@ import { parseSchema } from "../utils/parse-schema.js";
 
 const MaxRetryDuration = Schema.NumberFromString;
 
-export const command = Command.make(
-  "run",
-  {
-    args: Args.text({ name: "substream" }).pipe(
-      Args.withDescription("The path to a substream package (.spkg) or substreams.yaml file"),
-      Args.withDefault("substreams.yaml"),
+const config = {
+  args: Args.text({ name: "substream" }).pipe(
+    Args.withDescription("The path to a substream package (.spkg) or substreams.yaml file"),
+    Args.withDefault("substreams.yaml"),
+  ),
+  options: Options.all({
+    outputModule: Options.text("output-module").pipe(
+      Options.withAlias("o"),
+      Options.withDescription("Output module name"),
     ),
-    options: Options.all({
-      outputModule: Options.text("output-module").pipe(
-        Options.withAlias("o"),
-        Options.withDescription("Output module name"),
+    params: Options.keyValueMap("params").pipe(
+      Options.optional,
+      Options.withAlias("p"),
+      Options.withDescription(
+        "Set params for parameterizable modules in the form of `-p <module>=<value>`. Can be specified multiple times (e.g. `-p module1=valA -p module2=valX&valY`)",
       ),
-      params: Options.keyValueMap("params").pipe(
-        Options.optional,
-        Options.withAlias("p"),
-        Options.withDescription(
-          "Set params for parameterizable modules in the form of `-p <module>=<value>`. Can be specified multiple times (e.g. `-p module1=valA -p module2=valX&valY`)",
-        ),
+    ),
+    endpoint: Options.text("endpoint").pipe(
+      Options.withAlias("e"),
+      Options.withDescription("Endpoint to connect to"),
+      Options.withDefault("https://mainnet.eth.streamingfast.io"),
+    ),
+    finalBlocksOnly: Options.boolean("final-blocks-only").pipe(
+      Options.withDescription("Get only irreversible blocks"),
+      Options.withDefault(false),
+    ),
+    developmentMode: Options.boolean("development-mode").pipe(
+      Options.withDescription(
+        "Enable development mode, use it for testing purpose only, should not be used for production workload",
       ),
-      endpoint: Options.text("endpoint").pipe(
-        Options.withAlias("e"),
-        Options.withDescription("Endpoint to connect to"),
-        Options.withDefault("https://mainnet.eth.streamingfast.io"),
-      ),
-      finalBlocksOnly: Options.boolean("final-blocks-only").pipe(
-        Options.withDescription("Get only irreversible blocks"),
-        Options.withDefault(false),
-      ),
-      developmentMode: Options.boolean("development-mode").pipe(
-        Options.withDescription(
-          "Enable development mode, use it for testing purpose only, should not be used for production workload",
-        ),
-        Options.withDefault(false),
-      ),
-      maxRetryDuration: Options.text("max-retry-duration").pipe(
-        Options.withDescription("Maximum duration to retry for in seconds"),
-        Options.withDefault("300"),
-        Options.mapOrFail(parseSchema(MaxRetryDuration)),
-        Options.map(Duration.seconds),
-      ),
-    }),
-  },
-  ({ options, args: packagePath }) => {
-    const stream = Effect.gen(function* (_) {
-      if (!(packagePath.startsWith("http://") || packagePath.startsWith("https://"))) {
-        const path = yield* _(Path.Path);
-        if (!path.isAbsolute(packagePath)) {
-          packagePath = path.join(process.cwd(), packagePath);
-        }
-      }
+      Options.withDefault(false),
+    ),
+    maxRetryDuration: Options.text("max-retry-duration").pipe(
+      Options.withDescription("Maximum duration to retry for in seconds"),
+      Options.withDefault("300"),
+      Options.mapOrFail(parseSchema(MaxRetryDuration)),
+      Options.map(Duration.seconds),
+    ),
+  }),
+};
 
-      return yield* _(
-        Stream.runStream({
-          packagePath,
-          outputModule: options.outputModule,
-        }),
-      );
+export const command = Command.make("run", config, ({ options, args: packagePath }) => {
+  return Effect.gen(function* (_) {
+    if (!(packagePath.startsWith("http://") || packagePath.startsWith("https://"))) {
+      const path = yield* _(Path.Path);
+      if (!path.isAbsolute(packagePath)) {
+        packagePath = path.join(process.cwd(), packagePath);
+      }
+    }
+
+    const stream = Stream.runStream({
+      packagePath,
+      outputModule: options.outputModule,
     });
 
-    return Effect.provide(stream, Layer.merge(Stream.layer, Path.layer));
-  },
-);
+    return yield* _(Effect.provide(stream, Layer.merge(Stream.layer, Path.layer)));
+  });
+});
