4bfdadfe3d36c0d4301f61e0c667a07dcd1cc1a1
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2024-12-12 16:24:31 -0500

    hotfix for protofiles that are disordered

--- packages/core/src/utils/create-registry.ts
+++ packages/core/src/utils/create-registry.ts
@@ -2,5 +2,21 @@ import { type IMessageTypeRegistry, createDescriptorSet, createRegistryFromDescr
 import type { Package } from "../proto.js";
 
 export function createRegistry(substream: Package): IMessageTypeRegistry {
+  let reordered = [];
+    for (let i = 0; i < substream.protoFiles.length; i++) {
+        let file = substream.protoFiles[i];
+        if (file.name === "google/protobuf/any.proto") {
+            reordered.push(file);
+            break;
+        }
+    }
+    for (let i = 0; i < substream.protoFiles.length; i++) {
+        let file = substream.protoFiles[i];
+        if (file.name === "google/protobuf/any.proto") {
+            continue;
+        }
+        reordered.push(file);
+    }
+
   return createRegistryFromDescriptors(createDescriptorSet(substream.protoFiles), true);
 }
